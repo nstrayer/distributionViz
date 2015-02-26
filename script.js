2bdf77b551c8338da648c5f985ee7eaae57abdac
Nick Strayer <nstrayer@uvm.edu>
2015-02-25 23:34:01 -0500

    Added a mouseover for transition effect

--- script.js
+++ script.js
@@ -1,11 +1,6 @@
 // //make the distribution data
 //http://stackoverflow.com/questions/8689498/drawing-multiple-lines-in-d3-js
 
-var logistic = function(x, theta) {
-    var mu = 0;
-    var y = (1 / (Math.sqrt(2 * Math.PI) * theta)) * (1 / x) * Math.exp(-Math.pow((Math.log(x) - mu), 2) / (2 * Math.pow(theta, 2)))
-    return y;
-}
 
 var margin = {
         top: 30,
@@ -16,13 +11,19 @@ var margin = {
     width = 1000 - margin.left - margin.right,
     height = 500 - margin.top - margin.bottom;
 
+var logistic = function(x, theta) {
+    var mu = 0;
+    var y = (1 / (Math.sqrt(2 * Math.PI) * theta)) * (1 / x) * Math.exp(-Math.pow((Math.log(x) - mu), 2) / (2 * Math.pow(theta, 2)))
+    return y;
+}
+
 xs = _.range(0.01, 5, .01)
 
 thetaMap = d3.scale.linear() //name the values from 0 to 20 and make their values from .1-.7
     .domain([0, 20])
     .range([0.1, 0.7])
 
-var numOfLines = 30;
+var numOfLines = 20;
 
 var yPos = d3.scale.linear() //scalling for creating horizontal lines
     .domain([0, numOfLines])
@@ -30,8 +31,7 @@ var yPos = d3.scale.linear() //scalling for creating horizontal lines
 
 
 //default flat lines:
-
-dataTest = _.map(d3.range(numOfLines), function(i){
+logistic = _.map(d3.range(numOfLines), function(i){
 	toReturn = _.map(xs, function(num) {
         return {
             "x": num,
@@ -42,7 +42,7 @@ dataTest = _.map(d3.range(numOfLines), function(i){
     return toReturn;
 })
 
-dataStart = _.map(d3.range(numOfLines), function(i){
+horizontal = _.map(d3.range(numOfLines), function(i){
 	toReturn = _.map(xs, function(num) {
         return {
             "x": num,
@@ -77,76 +77,22 @@ var svg = d3.select("body").append("svg")
     .append("g")
     .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
 
-var group = svg.append("g")
+function change(newData){
+	svg.selectAll(".line")
+	    .data(newData)
+	    .transition()
+		.duration(2000)
+		.delay(function(d,i){return 30*i})
+	    .attr("class", "line")
+	    .attr("d", line);
+}
+
 
 svg.selectAll(".line")
-    .data(dataStart)
+    .data(horizontal)
     .enter().append("path")
     .attr("class", "line")
-    .attr("d", line);
-
-// svg.selectAll(".line")
-//     .data(dataTest)
-//     .transition()
-// 	.duration(2000)
-// 	.delay(function(d,i){return 30*i})
-//     .attr("class", "line")
-//     .attr("d", line);
-
-//
-// var x = d3.scale.linear()
-//     .range([0, width]);
-//
-// var y = d3.scale.linear()
-//     .range([height, 0]);
-//
-// var xAxis = d3.svg.axis()
-//     .scale(x)
-//     .orient("bottom");
-//
-// var yAxis = d3.svg.axis()
-//     .scale(y)
-//     .orient("left");
-//
-//
-// var svg = d3.select("body").append("svg")
-//     .attr("width", width + margin.left + margin.right)
-//     .attr("height", height + margin.top + margin.bottom)
-//     .append("g")
-//     .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
-//
-//
-// x.domain(d3.extent(dataTest, function(d) {
-//     return d.x;
-// }));
-//
-// y.domain([0,5]);
-//
-// svg.append("g")
-//     .attr("class", "x axis")
-//     .attr("transform", "translate(0," + height + ")")
-//     .call(xAxis);
-//
-// svg.append("g")
-//     .attr("class", "y axis")
-//     .call(yAxis)
-//
-//
-// for (i in d3.range(numOfLines)){
-//
-// 	var line = d3.svg.line()
-// 	    .x(function(d) {
-// 	        return x(d.x);
-// 	    })
-// 	    .y(function(d) {
-// 	        return y(d[i]);
-// 	    });
-//
-// 	svg.append("path")
-// 	    .datum(dataTest)
-// 	    .attr("class", "line")
-// 	    .attr("d", line)
-// 		.on("mouseover", function(d){
-// 			d3.select(this).attr("fill", "red")
-// 		});
-// }
+    .attr("d", line)
+	.on("mouseover", function(){
+		change(logistic)
+	});
